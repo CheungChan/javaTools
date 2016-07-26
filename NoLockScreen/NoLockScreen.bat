ff92fafe882414e014ea1b742a0f1ac39877da12
cheungchan <1377699408@qq.com>
2016-07-26 22:56:17 +0800

    启动时隐藏运行

--- NoLockScreen/NoLockScreen.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title No Lockscreen Program
-echo No lockscreen program is running.Please dou't turn off!!!!
-javac NoLockScreen.java
-java NoLockScreen
---
+++ NoLockScreen/start.bat
@@ -0,0 +1,7 @@
+@echo off
+if "%1" == "h" goto begin 
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
+:begin 
+echo No lockscreen program is running.Please dou't turn off!!!!
+javac NoLockScreen.java
+java NoLockScreen
