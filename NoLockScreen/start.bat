9f68b282ec41333d3762178158727ebc9d7eb83b
陈章 <1377699408@qq.com>
2016-07-27 11:41:21 +0800

    修改文件路径bug

--- NoLockScreen/start.bat
+++ NoLockScreen/start.bat
@@ -1,7 +1,8 @@
 @echo off
-if "%1" == "h" goto begin 
-mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
-:begin 
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
+:begin
 echo No lockscreen program is running.Please dou't turn off!!!!
+cd src
 javac NoLockScreen.java
 java NoLockScreen
